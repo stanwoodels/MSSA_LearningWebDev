09b348b3da5edb114eb4dea0f3976fffc9b59717
Tristan Stickels <tristan.stickels@gmail.com>
2022-03-14 15:23:56 +0100

    Added some examples and linked them from index.

--- js/main.js
+++ js/main.js
@@ -2,3 +2,6 @@ function myTest() {
     alert("Hello -- I'm JS, and I'm working!");
     console.log("This is better because it doesn't pop up!");
 }
+
+let str = "Apple, Banana, Kiwi";
+document.getElementById("demoSubstring").innerHTML = str.substring(7, 13);
